81840ee3c97418befeee2733350ed4017228a592
Lars Wobus <lars.wobus@web.de>
2018-12-02 15:13:16 +0100

    Custom inspector for SceneLightingSpecification.cs

---
+++ LightingEditor/Assets/EssentialPackages/LightingEditor/Scripts/Editor/SceneLightingSpecificationInspector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+namespace EssentialPackages.LightingEditor.Editor
+{
+    [CustomEditor(typeof(SceneLightingSpecification))]
+    public class SceneLightingSpecificationInspector : UnityEditor.Editor
+    {
+        private void OnEnable()
+        {
+            
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+        }
+    }
+}
